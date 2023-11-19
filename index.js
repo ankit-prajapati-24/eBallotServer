9b33221e998de3e3339785ed11965a3bd4c47632
ankit-prajapati-24 <141832510+ankit-prajapati-24@users.noreply.github.com>
2023-11-19 19:20:08 +0530

    update index  commit

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const app = express();
 
-// const userRoutes = require("./routes/User");
-// const profileRoutes = require("./routes/Profile");
-// const ElectionsRoute = require("./routes/ElectionsRoute");
-// const SurveyRoute = require("./routes/SurveyRoute");
-// const dbconnect = require("./config/database");
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const ElectionsRoute = require("./routes/ElectionsRoute");
+const SurveyRoute = require("./routes/SurveyRoute");
+const dbconnect = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-// const { ConnectCloadinary } = require("./config/Coudinary");
+const { ConnectCloadinary } = require("./config/Coudinary");
 const fileupload = require("express-fileupload");
 const dotenv = require("dotenv");
 dotenv.config(); // Load environment variables from .env file
 const PORT = process.env.PORT || 4000;
 
 // connect database
-// dbconnect();♣
+dbconnect();
 
 // middleware
 const bodyparser = require("body-parser");
@@ -37,9 +37,9 @@ app.use(
   })
 );
 
-// ConnectCloadinary();
+ConnectCloadinary();
 
-// app.use("/api/v1/auth", userRoutes); // Add a leading slash to the routes
+app.use("/api/v1/auth", userRoutes); // Add a leading slash to the routes
 // app.use("/api/v1/Services", ElectionsRoute);
 // app.use("/api/v1/profile", profileRoutes);
 // app.use("/api/v1/Survey", SurveyRoute);
